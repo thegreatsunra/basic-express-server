b618a4a2c53b7df1cc9255c3b91b79b0fc84c31c
Dane Petersen <thegreatsunra@gmail.com>
2017-07-24 14:23:29 -0500

    Query db and log values from it

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const { Client } = require('pg')
+const client = new Client({
+  database: 'testdb'
+})
 const app = express()
+
+async function db() {
+  await client.connect()
+
+  const res = await client.query('SELECT * from test_table')
+  console.log(res.rows) // Hello world!
+  await client.end()
+}
 
 app.use(express.static('public'))
 
 app.listen(3000, () => {
   console.log('Express listening on port 3000')
 })
+
+db()
