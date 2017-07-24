e9db45524c112f6bf42e6db1eae9b7f89954f672
Dane Petersen <thegreatsunra@gmail.com>
2017-07-24 16:14:39 -0500

    Remove DB cruft from app index

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 const express = require('express')
-const { Client } = require('pg')
-const client = new Client({
-  database: 'testdb'
-})
 const morgan = require('morgan')
 const app = express()
 const port = process.env.PORT || 8080
 
-async function db() {
-  await client.connect()
-
-  const res = await client.query('SELECT * from test_table')
-  console.log(res.rows) // Hello world!
-  await client.end()
-}
-
-app.use(express.static('public'))
 app.use(morgan('dev'))
 
 app.listen(port)
