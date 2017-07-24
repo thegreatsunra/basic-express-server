a29f83046784dd59484f9b5c5c8eb3f5be989408
Dane Petersen <thegreatsunra@gmail.com>
2017-07-24 16:14:58 -0500

    Include and use new routes architecture

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const routes = require('./server/routes')
 const app = express()
+routes(app)
 const port = process.env.PORT || 8080
 
 app.use(morgan('dev'))
 
 app.listen(port)
-console.log('Magic happens on port ' + port)
+console.log('Magic happening on port ' + port)
